ea80897afb66d28c498b20b37d57d69a2df93761
Jonathon Silva Eng <54660634+jonathon-eng@users.noreply.github.com>
2022-09-28 18:28:36 -0700

    updates

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const rating = document.getElementsByClassName("rating");
+const firstCard = document.getElementsByClassName("Rating-state-card");
 
-function getRating() {
-    console.log(this.rating, this.innerHTML);
+const secondCard = document.getElementsByClassName("Thank-you-card");
+let data = document.querySelector('input[name="select"]:checked');
+
+
+
+function displayData() {
+    data = document.querySelector('input[name="select"]:checked').value;
+    document.getElementById("selection1").innerHTML = data;
+    event.preventDefault();
+    document.getElementById("card1").style.display="none";
+    document.getElementById("card2").style.display="block";
 }
+
